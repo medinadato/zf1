a32ed07173622908e00c11653ba65802572006d3
Medina PC <medinadato@gmail.com>
2012-09-19 22:40:21 -0300

    Forms and stuffs

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -7,5 +7,15 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         Zend_Registry::set('config', $config);
     }
 
+    public function _initSession() {
+        $session = new Zend_Session_Namespace('blog');
+        Zend_Registry::set('session', $session);
+    }
+    
+    public function _initDb() {
+        $db = $this->getPluginResource('db')->getDbAdapter();
+        Zend_Db_Table::setDefaultAdapter($db);
+        Zend_Registry::set('db', $db);
+    }
 }
 
--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -2,13 +2,12 @@
 
 class IndexController extends Zend_Controller_Action
 {
-
-    public function init()
-    {
-        /* Initialize action controller here */
-    }
-
     public function indexAction()
+    {
+        $this->view->form = new Application_Form_Login;
+    }
+    
+    public function showAction()
     {
         // action body
     }
